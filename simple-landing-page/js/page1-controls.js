4f59159d5ceecfb8b2a5172a8e87b4d2f3ac4d06
econdorir <118948322+econdorir@users.noreply.github.com>
2024-06-14 13:16:24 -0400

    Delete simple-landing-page directory

--- simple-landing-page/js/page1-controls.js
+++
@@ -1,5 +0,0 @@
-const buttonToReadIntroduction = document.querySelector("#button-to-read-introduction");
-buttonToReadIntroduction.addEventListener("click", (event) => {
-    event.preventDefault()
-    console.log("hola");
-})
